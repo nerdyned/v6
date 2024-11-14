fc05441584776fac83f70c4e7b8385b1ea926538
willo <153154914+willoo0@users.noreply.github.com>
2024-11-13 23:29:00 -0600

    Update embed.js

--- public/static/embed.js
+++ public/static/embed.js
@@ -2,21 +2,32 @@
 let destination = "";
 
 try {
+
   let hashValue = location.hash.slice(1);
   let decodedHashValue = decodeURIComponent(hashValue);
+
+  if (!decodedHashValue.startsWith("http://") && !decodedHashValue.startsWith("https://")) {
+    decodedHashValue = "https://" + decodedHashValue;
+  }
+
   destination = new URL(decodedHashValue).toString();
 } catch (err) {
-  alert(`Invalid # string or Invalid URL. Got error:\n${err}`);
+  alert(`Invalid URL format in hash. Error:\n${err}`);
   throw err;
 }
 
-registerSW()
-  .then(() => {
+async function registerSW() {
+  try {
+    await window.navigator.serviceWorker.register("/sw.js", {
+      scope: __uv$config.prefix,
+    });
     window.open(
       __uv$config.prefix + __uv$config.encodeUrl(destination),
       "_self"
     );
-  })
-  .catch((err) => {
-    alert(`Encountered error:\n${err}`);
-  });
+  } catch (err) {
+    alert(`Failed to register Service Worker or redirect. Error:\n${err}`);
+  }
+}
+
+registerSW();
