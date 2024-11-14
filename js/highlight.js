fa4267a4e9cb8b308cce2b84de4d7fdc2c09fcb6
willo <153154914+willoo0@users.noreply.github.com>
2024-11-13 20:58:15 -0600

    Delete js directory

--- js/highlight.js
+++
@@ -1,8 +0,0 @@
-const style = document.createElement('style');
-style.innerHTML = `
-    ::selection {
-        background-color: #C93131;
-        color: white; 
-    }
-`;
-document.head.appendChild(style);
