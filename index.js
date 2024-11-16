b8f5bde60952854c7fb020736cfd86910dc3c3e7
willo <153154914+willoo0@users.noreply.github.com>
2024-11-15 21:17:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-const http = require('node:http');
-const { createBareServer } = require('@tomphttp/bare-server-node');
+const http = require('node:http')
+const { createBareServer } = require('@tomphttp/bare-server-node')
 const express = require('express');
-const path = require('path');
 const sqlite = require('sqlite3').verbose();
-
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
+const httpServer = http.createServer();
 const bareServer = createBareServer("/bare/");
 
-const httpServer = http.createServer((req, res) => {
-
+httpServer.on("request", (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
-  
     app(req, res);
   }
 });
 
-httpServer.on('upgrade', (req, socket, head) => {
+httpServer.on("upgrade", (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
     socket.end();
   }
 });
-
-app.use((req, res) => {
- 
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
 const port = 2100;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+httpServer.on("listening", () => {
+  console.log("HTTP server listening");
+  console.log(`View your server at http://localhost:${port}`);
+});
+
+httpServer.listen({
+  port: port,
+});
