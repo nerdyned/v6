e2bf76bf5433487e8e8cea322be468c9ccda4f77
willo <153154914+willoo0@users.noreply.github.com>
2024-11-15 20:18:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const http = require('node:http')
-const { createBareServer } = require('@tomphttp/bare-server-node')
+const http = require('node:http');
+const { createBareServer } = require('@tomphttp/bare-server-node');
 const express = require('express');
+const path = require('path');
 const sqlite = require('sqlite3').verbose();
+
 const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
-
-// Create an HTTP server
 const httpServer = http.createServer();
+
 const bareServer = createBareServer("/bare/");
 
-
-
 httpServer.on("request", (req, res) => {
+  
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
+  
     app(req, res);
   }
 });
@@ -31,6 +31,11 @@ httpServer.on("upgrade", (req, socket, head) => {
   } else {
     socket.end();
   }
+});
+
+app.use((req, res) => {
+ 
+  res.status(404).sendFile(path.join(__dirname, 'public', '/public/404.html'));
 });
 const port = 2100;
 
