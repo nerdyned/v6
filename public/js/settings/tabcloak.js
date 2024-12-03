1e84e17ac5efa3842df2188d60c6152e4098beb5
willo <153154914+willoo0@users.noreply.github.com>
2024-12-02 21:22:13 -0600

    Update tabcloak.js

--- public/js/settings/tabcloak.js
+++ public/js/settings/tabcloak.js
@@ -1,31 +1,47 @@
 function handleClick(box, tabName, favicon) {
-box.classList.add('clicked');
-document.title = tabName;
-const faviconLink = document.querySelector("link[rel*='icon']");
-faviconLink.href = favicon;
-localStorage.setItem('tabName', tabName);
-localStorage.setItem('favicon', favicon);
-setTimeout(() => {
-    box.classList.remove('clicked');
-}, 500);
+    box.classList.add('clicked');
+    document.title = tabName;
+    const faviconLink = document.querySelector("link[rel*='icon']");
+    
+    if (faviconLink) {
+        faviconLink.href = favicon;
+        localStorage.setItem('tabName', tabName);
+        localStorage.setItem('favicon', favicon);
+    } else {
+        console.error('Favicon link tag not found in the document. Please add <link rel="icon" href="/images/plexilearcade.png" /> in the <head>.');
+    }
+
+    setTimeout(() => {
+        box.classList.remove('clicked');
+    }, 500);
 }
 
 function resetCloak() {
-const originalTabName = '';
-const originalFavicon = '/plexilearcade.png';
-document.title = originalTabName;
-const faviconLink = document.querySelector("link[rel*='icon']");
-faviconLink.href = originalFavicon;
-localStorage.setItem('tabName', originalTabName);
-localStorage.setItem('favicon', originalFavicon);
+    const originalTabName = '';
+    const originalFavicon = '/plexilearcade.png';
+    document.title = originalTabName;
+    const faviconLink = document.querySelector("link[rel*='icon']");
+    
+    if (faviconLink) {
+        faviconLink.href = originalFavicon;
+        localStorage.setItem('tabName', originalTabName);
+        localStorage.setItem('favicon', originalFavicon);
+    } else {
+        console.error('Favicon link tag not found in the document. Please add <link rel="icon" href="/images/plexilearcade.png" /> in the <head>.');
+    }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-const savedTabName = localStorage.getItem('tabName');
-const savedFavicon = localStorage.getItem('favicon');
-if (savedTabName && savedFavicon) {
-    document.title = savedTabName;
+    const savedTabName = localStorage.getItem('tabName');
+    const savedFavicon = localStorage.getItem('favicon');
     const faviconLink = document.querySelector("link[rel*='icon']");
-    faviconLink.href = savedFavicon;
-}
+    
+    if (faviconLink) {
+        if (savedTabName && savedFavicon) {
+            document.title = savedTabName;
+            faviconLink.href = savedFavicon;
+        }
+    } else {
+        console.error('Favicon link tag not found in the document. Please add <link rel="icon" href="/images/plexilearcade.png" /> in the <head>.');
+    }
 });
