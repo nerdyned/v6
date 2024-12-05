17cf5e1b5c7cab4610158238e92ea6834125466b
willo <153154914+willoo0@users.noreply.github.com>
2024-12-04 20:57:15 -0600

    Create 404.js

---
+++ public/js/404.js
@@ -0,0 +1,7 @@
+const frameWeb = document.getElementById("frameWeb");
+const gameFrame = document.getElementById("gameFrame");
+
+if (frameWeb || gameFrame) {
+  
+  window.location.href = "/static/404.html";
+}
