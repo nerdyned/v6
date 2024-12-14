ef2e7cf2fd331922a6487acb100848e206ccff03
willo <153154914+willoo0@users.noreply.github.com>
2024-12-14 16:39:25 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,61 @@ const shortcuts = {
     "!yt": "https://www.youtube.com",
     "!gh": "https://github.com",
     "!rd": "https://www.reddit.com",
-    "!ddg": "https://duckduckgo.com",
     "!tw": "https://twitter.com",
+    "!fb": "https://www.facebook.com",
+    "!ig": "https://www.instagram.com",
+    "!li": "https://www.linkedin.com",
+
+    "!pa": "https://plexilearcade.xyz",
+    "!xn": "https://xenapsis.dev",
+    "!cg": "https://crazygames.com",
+    "!p": "https://poki.com",
+    "!s": "https://selenite.cc",
+
+    "!nyt": "https://www.nytimes.com",
+    "!cnn": "https://www.cnn.com",
+    "!bbc": "https://www.bbc.com",
+    "!wp": "https://www.washingtonpost.com",
+
+    "!amz": "https://www.amazon.com",
+    "!eb": "https://www.ebay.com",
+    "!et": "https://www.etsy.com",
+    "!wm": "https://www.walmart.com",
+
+    "!ms": "https://www.microsoft.com",
+    "!ap": "https://www.apple.com",
+    "!st": "https://stackoverflow.com",
+
+    "!nf": "https://www.netflix.com",
+    "!hb": "https://www.hbomax.com",
+    "!sp": "https://open.spotify.com",
+    "!dc": "https://www.discord.com",
+
+    "!kh": "https://www.khanacademy.org",
+    "!edx": "https://www.edx.org",
+    "!ud": "https://www.udemy.com",
+    "!cs": "https://www.codecademy.com",
+
+    "!npm": "https://www.npmjs.com",
+    "!jsf": "https://jsfiddle.net",
+    "!mdn": "https://developer.mozilla.org",
+    "!dev": "https://dev.to",
+
+    "!pt": "https://www.pinterest.com",
+    "!tk": "https://www.tiktok.com",
+    "!sc": "https://www.snapchat.com",
+    "!tm": "https://www.tumblr.com",
+
+    "!g": "https://www.google.com",
+    "!b": "https://www.bing.com",
+    "!y": "https://search.yahoo.com",
+    "!ba": "https://www.baidu.com",
+    "!ddg": "https://duckduckgo.com",
+
+    "!wiki": "https://en.wikipedia.org",
+    "!imdb": "https://www.imdb.com",
+    "!gm": "https://www.google.com/maps",
+    "!oo": "https://www.onlyoffice.com",
 };
 
 form.addEventListener('submit', async event => {
