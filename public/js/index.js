da2628624b6535e7d5ed2c89aa4a4a5bb4c8b31b
willo <153154914+willoo0@users.noreply.github.com>
2024-12-14 16:26:17 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
 const form = document.querySelector('#uv-form');
 const input = document.querySelector('#uv-address'); 
 
+const shortcuts = {
+    "!yt": "https://www.youtube.com",
+    "!gh": "https://github.com",
+    "!rd": "https://www.reddit.com",
+    "!ddg": "https://duckduckgo.com",
+    "!tw": "https://twitter.com",
+};
+
 form.addEventListener('submit', async event => {
     event.preventDefault();
     
@@ -8,8 +16,17 @@ form.addEventListener('submit', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+        
+        const shortcutKey = url.split(" ")[0]; 
+        if (shortcuts[shortcutKey]) {
+            url = shortcuts[shortcutKey]; 
+        } else if (!isUrl(url)) {
+        
+            url = 'https://www.google.com/search?q=' + url;
+        } else if (!(url.startsWith('https://') || url.startsWith('http://'))) {
+           
+            url = 'http://' + url;
+        }
 
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
